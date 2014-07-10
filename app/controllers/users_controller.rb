9da7340b407d5ee4ff1946a79ba0824ab76d9aa8
David Ng <davidngcf@gmail.com>
2014-07-10 16:37:07 +0800

    add application helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:index]
+
   def index
     @users = User.all
   end
@@ -14,6 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user, notice: "Thanks for signing up!"
     else
       render :new
@@ -21,11 +26,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: "Account Successfully updated!"
     else
@@ -34,8 +38,8 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
+    session[:user_id] = nil
     redirect_to root_path, alert: "Account successfully deleted"
   end
 
@@ -44,4 +48,13 @@ private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
+  def require_correct_user
+    @user = User.find(params[:id])
+    unless current_user?(@user)
+      redirect_to root_url
+    end
+  end
+
+
 end
