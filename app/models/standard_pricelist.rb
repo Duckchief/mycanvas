9e98702935eb7ea88181d57e2ea826fcd1e8c776
David Ng <davidngcf@gmail.com>
2014-08-04 16:09:07 +0800

    simplified get started page

--- app/models/standard_pricelist.rb
+++ app/models/standard_pricelist.rb
@@ -14,6 +14,6 @@ class StandardPricelist < ActiveRecord::Base
   has_many :shopping_cart_items
 
   def to_s
-    "#{width} x #{height} $#{(price)}"
+    "#{width.to_i}\" x #{height.to_i}\" $#{(price)}"
   end
 end
